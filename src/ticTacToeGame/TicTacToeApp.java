78fb65b76c673f492704aaf53537b5348d9d9500
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-08 01:16:34 +0100

    tictaktoe game updated

--- src/ticTacToeGame/TicTacToeApp.java
+++ src/ticTacToeGame/TicTacToeApp.java
@@ -1,7 +1,39 @@
 package ticTacToeGame;
 
+import java.util.Scanner;
+
 public class TicTacToeApp {
     public static void main(String[] args) {
+
+        Board ticTakToeBoard = new Board(3);
+        TicTacToe myTikky = new TicTacToe(ticTakToeBoard);
+
+        Scanner input = new Scanner(System.in);
+
+        boolean gameOver = false;
+
+        while (!gameOver){
+
+            try {
+                myTikky.displayBoard();
+                System.out.println("Enter a position to play fron 1-9");
+                int position = input.nextInt();
+                myTikky.markBoard(position);
+
+            } catch (GameOverException e) {
+                if ( myTikky.checkIfGameIsWon() && myTikky.lastPlayerToPlayWasX){
+                    System.out.println("Player X WINS " + myTikky.checkIfGameIsWon());
+                    myTikky.displayBoard();
+                    System.exit(0);
+                }else{
+                    System.out.println("Player O WINS " + myTikky.checkIfGameIsWon());
+                    System.exit(0);
+                }
+                    gameOver = true;
+            }
+
+        }
+
 
     }
 }
