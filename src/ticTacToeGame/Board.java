5cce40656a19380cbe48c63e9f227455ddc08c78
Oziichukwu <ukaegbugoodnews13@gmail.com>
2021-11-08 01:15:36 +0100

    tictaktoe game updated

--- src/ticTacToeGame/Board.java
+++ src/ticTacToeGame/Board.java
@@ -1,2 +1,27 @@
-package ticTacToeGame;public class Board {
+package ticTacToeGame;
+
+import java.util.Arrays;
+
+public class Board {
+    private TicTacToeValue [][]ticTacToeGrid;
+
+    public Board(int gridNumber) {
+        ticTacToeGrid = new TicTacToeValue[gridNumber][gridNumber];
+        for (int row = 0; row < ticTacToeGrid.length; row++) {
+            Arrays.fill(ticTacToeGrid[row], TicTacToeValue.Empty);
+        }
+    }
+
+    public TicTacToeValue[][] getGrid() {
+        return ticTacToeGrid;
+    }
+
+    public void displayBoard() {
+        for (int row = 0; row < ticTacToeGrid.length; row++) {
+            for (int column = 0; column < ticTacToeGrid.length; column++) {
+                System.out.print(ticTacToeGrid[row][column] + "| ");
+            }
+            System.out.println();
+        }
+    }
 }
